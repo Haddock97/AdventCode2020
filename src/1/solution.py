5bbe0442fe2533a6e324fae5a07cb718dee570f8
Sidd L <sidd.lakshman@gmail.com>
2020-12-01 19:50:44 -0500

    Almost correct solution

--- src/1/solution.py
+++ src/1/solution.py
@@ -1,1 +1,12 @@
 #advent code challenge day 1 problem 1
+
+import sys
+s = {}
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        l = int(line)
+        if (2020 - l) in s: 
+            print(l*(2020-l))
+            break
+        else:
+            s.add(2020-l)
