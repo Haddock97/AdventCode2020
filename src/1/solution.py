0128c47530f0520f32e2b25243ea4688ab958fd7
Sidd L <sidd.lakshman@gmail.com>
2020-12-01 19:56:03 -0500

    correct solution maybe sidd

--- src/1/solution.py
+++ src/1/solution.py
@@ -1,11 +1,11 @@
 #advent code challenge day 1 problem 1
 
 import sys
-s = {}
+s = set()
 with open(sys.argv[1], 'r') as f:
     for line in f:
         l = int(line)
-        if (2020 - l) in s: 
+        if (l) in s: 
             print(l*(2020-l))
             break
         else:
