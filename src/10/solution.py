f94da3f67814621d47a7f86ae7ebf3b766cbdf00
Mukund Lakshman <yaymukund@gmail.com>
2020-12-13 19:22:33 +0000

    Push mukund's solution for 10

---
+++ src/10/solution.py
@@ -0,0 +1,28 @@
+import sys
+
+LETTER_TO_BINARY = {
+    "F": "0",
+    "B": "1",
+    "L": "0",
+    "R": "1",
+}
+ 
+def seat_to_binary(seat):
+    seat = seat.strip()
+    binary_string = [LETTER_TO_BINARY[letter] for letter in seat]
+    binary_string = ''.join(binary_string) # ['0', '1', '1', '1'] => '0111'
+    return int(binary_string, 2)
+
+with open(sys.argv[1]) as f:
+    seat_numbers = [seat_to_binary(seat) for seat in f]
+
+SEAT_MIN = 8
+SEAT_MAX = (2**10) - 8
+
+for num in range(SEAT_MIN, SEAT_MAX):
+    if (
+        (num not in seat_numbers) and
+        (num - 1) in seat_numbers and
+        (num + 1) in seat_numbers
+    ):
+        print(num)
