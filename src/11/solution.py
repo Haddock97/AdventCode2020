11624e6d3827c0761e174ad25b8a99cb059eddfb
Mukund Lakshman <yaymukund@gmail.com>
2020-12-13 20:39:36 +0000

    Day 11 - Add solution.py

---
+++ src/11/solution.py
@@ -0,0 +1,12 @@
+import sys
+import string
+
+with open(sys.argv[1]) as f:
+    groups = (group for group in f.read().split('\n\n'))
+
+count = 0
+for group in groups:
+    questions = {c for c in group if c in string.ascii_lowercase}
+    count += len(questions)
+
+print(count)
