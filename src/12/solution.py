2fd7435c590b4b762c7d41bcf17383469eb44ee4
Mukund Lakshman <yaymukund@gmail.com>
2020-12-15 21:31:08 +0000

    Add mukund's solution 12

--- src/12/solution.py
+++ src/12/solution.py
@@ -1,26 +1,15 @@
 import sys
-import string
 
 with open(sys.argv[1]) as f:
-    groups = f.read().split('\n\n')
+    groups = []
+    for lines in f.read().split('\n\n'):
+        group = []
+        for line in lines.strip().split('\n'):
+            group.append(set(line))
+        groups.append(group)
 
 count = 0
 for group in groups:
-    questions = {c for c in group if c in string.ascii_lowercase}
-    count += len(questions)
+    count += len(set.intersection(*group))
 
 print(count)
-
-'''
-import sys
-
-with open(sys.argv[1]) as f:
-    read_data = f.read()
-
-responses = [entry.replace("\n", "") for entry in read_data.split("\n\n")]
-count = 0
-for entry in responses:
-    count += len(set(entry))
-
-print(count)
-'''
