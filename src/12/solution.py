b025692b2c2471b9e32597dc80538bb58f03a62c
Sidd L <sidd.lakshman@gmail.com>
2020-12-15 17:32:59 -0500

    Added my solution

--- src/12/solution.py
+++ src/12/solution.py
@@ -13,3 +13,22 @@ for group in groups:
     count += len(set.intersection(*group))
 
 print(count)
+
+'''
+import sys
+
+def conv(l):
+    for i in range(len(l)):
+        l[i] = set(l[i])
+    return l
+
+with open(sys.argv[1]) as f:
+    read_data = f.read()
+
+responses = [entry.split("\n") for entry in read_data.split("\n\n")]
+count = 0
+
+for entry in responses:
+    count += len(set.intersection(*l))
+print(count)
+'''
